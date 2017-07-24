191b3c38cfad43f0a60afa82d79da20c59ce667d
danieljoserm <danieljoserm@gmail.com>
2017-07-24 12:32:17 -0600

    creando json request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { FormsModule }   from '@angular/forms';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
-
+import { HttpModule } from '@angular/http';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import {RestPage} from '../pages/rest-page/rest-page';
@@ -22,7 +22,7 @@ import {RestPage} from '../pages/rest-page/rest-page';
    
   ],
   imports: [
-   
+       HttpModule,
     BrowserModule,
      FormsModule, 
     IonicModule.forRoot(MyApp),
