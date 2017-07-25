8292cd40f5b330f5cd0d6b9aa239c3eca298b34a
danieljoserm <danieljoserm@gmail.com>
2017-07-24 19:32:34 -0600

    funciona http request en un solo componente

--- src/pages/rest-page/rest-page.ts
+++ src/pages/rest-page/rest-page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'page-rest-page',
   templateUrl: 'rest-page.html',
@@ -10,6 +11,17 @@ import { Component } from '@angular/core';
 
 export class RestPage {
   
+   posts: any;
   texto = 'por ahora';
-  
+  constructor(public http: Http){
+
+this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
+  console.log("hola")
+        this.posts = data.data.children;
+    }, err => {
+        console.log("Oops!");
+    });
+
+  }
+
 }
